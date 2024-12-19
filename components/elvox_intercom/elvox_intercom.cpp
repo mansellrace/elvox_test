93ec0699338f3ce4569717bb5c20b0835fe88691
Francesco <mansellrace@gmail.com>
2024-12-19 12:49:42 +0100

    ancora più log

--- components/elvox_intercom/elvox_intercom.cpp
+++ components/elvox_intercom/elvox_intercom.cpp
@@ -505,7 +505,7 @@ void ElvoxComponent::sending_loop() {
   if (this->send_next_change == 0) { // analizza prossimo bit
     this->tx_pin_->digital_write(true);
     this->send_next_change = now + this->send_buffer[this->send_index];
-    ESP_LOGD(TAG, "Elvox: send_buffer %i: %i %i  now %i a %i", this->send_index, this->send_buffer[this->send_index], this->send_buffer[this->send_index + 1], now, this->send_next_change);
+    ESP_LOGD(TAG, "Elvox: send_buffer %i: %i %i  now %i a %i b %i", this->send_index, this->send_buffer[this->send_index], this->send_buffer[this->send_index + 1], now, this->send_next_change, this->send_next_change + this->send_buffer[this->send_index + 1]);
     while (this->send_next_change >= micros()) {
     // MODULAZIONE
     }
