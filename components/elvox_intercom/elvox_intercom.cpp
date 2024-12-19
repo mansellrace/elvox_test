3bad350c5f4ae77b4bf9517f3c56f6359ecff7e6
Francesco <mansellrace@gmail.com>
2024-12-19 16:27:55 +0100

    fix const

--- components/elvox_intercom/elvox_intercom.cpp
+++ components/elvox_intercom/elvox_intercom.cpp
@@ -448,7 +448,7 @@ void ElvoxComponent::send_command(ElvoxIntercomData data) {
   for (size_t i = 0; i < size; i += 2) {
     //this->send_buffer[i] = data.array[i];
 
-    const uint32_t init_time, change_time = micros();
+    uint32_t init_time, change_time = micros();
     while (micros() - data.array[i] >= init_time) {
       this->tx_pin_->digital_write(!this->tx_pin_->digital_read());
       while (change_time - micros() >= 9) {
